17daab98d34c687b7566001a64a8f1fcdd4745eb
Vitaly Turovsky <vital2580@icloud.com>
2021-09-01 01:14:33 +0300

    fix: trying to fix windows path issues

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import { join, resolve } from 'path'
+import { join, resolve, relative } from 'path'
 import * as jsonfile from 'jsonfile'
 import { PackageJson } from 'type-fest'
 import { createLogger, InlineConfig, UserConfig, UserConfigExport, send as serverSendContent, mergeConfig, loadEnv } from 'vite'
@@ -100,7 +100,7 @@ export const defineVitConfig = (userConfig: VitUserConfig = {}): UserConfigExpor
             process.env[envName] = value
         }
 
-        const logger = createLogger('warn')
+        const logger = createLogger()
 
         // https://vitejs.dev/config/
         const defaultConfig: InlineConfig = {
@@ -128,6 +128,7 @@ export const defineVitConfig = (userConfig: VitUserConfig = {}): UserConfigExpor
                             // In case of serving index html we don't call next(), so we need to manually implement indexHtmlMiddleware middleware
                             // https://github.com/vitejs/vite/blob/main/packages/vite/src/node/server/middlewares/indexHtml.ts#L149
                             server.middlewares.use(async function serverGeneratedIndexHtml(req, res, next) {
+                                // TODO-high logic with custom entryPoints still works very badly. Investiage in lower commits on macOS
                                 const url = req.url && cleanUrl(req.url)
                                 if (url !== '/index.html' || req.headers['sec-fetch-dest'] === 'script') return next()
                                 // TODO I wanted to keep index.html files in src/ folder
@@ -136,22 +137,30 @@ export const defineVitConfig = (userConfig: VitUserConfig = {}): UserConfigExpor
                                 if (fs.existsSync(resolve(fullRootPath, 'index.html'))) return next()
                                 if (fs.existsSync(resolve(fullRootPath, 'src/index.html'))) logger.warn('Move src/index.html to the root')
                                 const { entryPoint = 'src' } = userConfig
+                                // detect cases when entryPoint go upper (it's not allowed) e.g. entryPoint: ../index.ts or ./test/dir/../../../index.ts but ./test/dir/../index.ts is ok
+                                const relativePath = relative(fullRootPath, join(fullRootPath, entryPoint))
+                                logger.info(`unresolved relative path ${relativePath} (${userConfig.entryPoint === undefined ? 'default' : 'user config'})`)
+                                if (relativePath.startsWith('..')) throw new TypeError('Check entryPoint config option. Entry point must be within root')
+                                // TODO-high @high fix prettier. actually doesn't format this block
                                 const isDir = fs.lstatSync(join(fullRootPath, entryPoint)).isDirectory()
+                                if(isDir) logger.info(`entryPoint is directory. peforming search for index.ts or index.tsx`)
                                 /** only if dir */
-                                const fromSrc = (path: string) => join(fullRootPath, entryPoint, path)
-                                let resolvedEntryPoint: undefined | string = isDir ? undefined : join(fullRootPath, entryPoint);
-                                if (!resolvedEntryPoint) {
+                                const absoluteFromSrc = (path: string) => join(fullRootPath, entryPoint, path)
+                                let resolvedRelativeEntryPoint: undefined | string = isDir ? undefined : entryPoint;
+                                if (!resolvedRelativeEntryPoint) {
+                                    // only if entry point path is directory
                                     // TODO-low use posix extended
-                                    [fromSrc('index.tsx'), fromSrc('index.ts')].forEach(path => {
-                                        if (!fs.existsSync(path)) return
-                                        resolvedEntryPoint = path
+                                    ['index.tsx', 'index.ts'].forEach(relativePath => {
+                                        if (!fs.existsSync(absoluteFromSrc(relativePath))) return
+                                        resolvedRelativeEntryPoint = join(entryPoint, relativePath)
                                     })
                                 }
-                                if (!resolvedEntryPoint) {
-                                    logger.error(userConfig.entryPoint ? 'Custom entry point' + entryPoint + "not found" : 'Entry point not found. Either create your index.html in root or create index.ts or index.tsx in src/')
+                                logger.info(`resolved entry point: ${resolvedRelativeEntryPoint}`)
+                                if (!resolvedRelativeEntryPoint) {
+                                    logger.error(userConfig.entryPoint ? 'Custom entry point ' + entryPoint + " not found" : 'Entry point not found. Either create your index.html in root or create index.ts or index.tsx in src/')
                                     return next()
                                 }
-                                const html = await server.transformIndexHtml(url, getIndexHtml(resolvedEntryPoint), req.originalUrl)
+                                const html = await server.transformIndexHtml(url, getIndexHtml(resolvedRelativeEntryPoint), req.originalUrl)
                                 serverSendContent(req, res, html, 'html')
                             })
                         }
