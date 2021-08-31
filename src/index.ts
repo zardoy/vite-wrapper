e68a637bf4b55ff4fafa4f1a30cb25965f5e9caf
Vitaly Turovsky <vital2580@icloud.com>
2021-08-31 19:11:07 +0300

    feat: rename `windiConfig` to `defineWindiConfig` to better align with Vite philosophy

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as windiConfig } from './windi.config'
+export { default as defineWindiConfig } from './windi.config'
 // config to be used in vite.config.ts instead of Vite's defineConfig
 export { defineVitConfig } from './config'
